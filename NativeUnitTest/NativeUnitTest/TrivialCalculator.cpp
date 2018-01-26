ac312bd49c5fe109897829c803915ad412a521f0
fanjinchang <894020798@qq.com>
2018-01-26 11:45:52 +0800

    NativeUnitTest

---
+++ NativeUnitTest/NativeUnitTest/TrivialCalculator.cpp
@@ -0,0 +1,21 @@
+#include "TrivialCalculator.h"
+
+double TrivialCalculator::Add(double x, double y)
+{
+	return x + y;
+}
+
+double TrivialCalculator::Subtract(double x, double y)
+{
+	return x - y;
+}
+
+double TrivialCalculator::Multiply(double x, double y)
+{
+	return x * y;
+}
+
+double TrivialCalculator::Divide(double x, double y)
+{
+	return x / y;
+}
---
+++ NativeUnitTest/NativeUnitTest/TrivialCalculator.h
@@ -0,0 +1,8 @@
+class TrivialCalculator
+{
+public:
+	double Add(double x, double y);
+	double Subtract(double x, double y);
+	double Multiply(double x, double y);
+	double Divide(double x, double y);
+};
