760ce1740789ef93afca1fbc7c8a7b2cd7364c28
fanjinchang <894020798@qq.com>
2018-01-26 11:00:19 +0800

    args_ellipsis

--- Lua/Lua5.3/TestLua/TestLua.cpp
+++ Lua/Lua5.3/TestLua/TestLua.cpp
@@ -1,10 +1,16 @@
 // TestLua.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
 #include <iostream>
 using namespace std;
-#include <lua.hpp>
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
 
 int main()
 {
